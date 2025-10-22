74b6d474d241baae5471572e5d5f16e9e9552fda
haryo <emadekuncoro@gmail.com>
2025-10-22 09:53:46 +0700

    initial commit

---
+++ src/main/java/com/project/github_workflow/GithubWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.project.github_workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubWorkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/github_workflow/GithubWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.github_workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
